18396f44a23c7da6104df1b9038e015dc929f258
Joshua Hill <josh@jlhworkshop.com>
2019-07-31 17:48:47 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -15,8 +15,7 @@ def map_to_square(source_array)
 end
 
 def reduce_to_total(source_array, starting_point)
-  if starting_point = nil
-    starting_point = 0
+  if starting_point = nil then starting_point = 0
   new = starting_point
   i = 0
   while i < source_array.length do
